77baad629d19dd9246410c007d0c0cc324002d34
Saron <saronabebe2019@gmail.com>
2025-05-10 12:21:57 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import General from "./components/General.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <General />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/General.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "../styles/style.css";
+
+const General = () => {
+  const [generalInfo, setGeneralInfo] = useState({
+    name: "",
+    email: "",
+    phone: "",
+  });
+
+  return (
+    <div className="general-container">
+      <div className="input-label">
+        <label htmlFor="name">Name: </label>
+        <input
+          type="text"
+          id="name"
+          value={generalInfo.name}
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, name: e.target.value });
+          }}
+        />
+      </div>
+
+      <div className="input-label">
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          value={generalInfo.email}
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, email: e.target.value });
+          }}
+        />
+      </div>
+
+      <div className="input-label">
+        <label htmlFor="num">Phone Number: </label>
+        <input
+          type="number"
+          id="num"
+          value={generalInfo.phone}
+          onChange={(e) => {
+            setGeneralInfo({ ...generalInfo, phone: e.target.value });
+          }}
+        />
+      </div>
+
+      <button type="Submit">Submit</button>
+    </div>
+  );
+};
+
+export default General;
