dcbed587d72e7d0a6df5561149ecaef50aed6f6e
Saron <saronabebe2019@gmail.com>
2025-05-10 18:05:20 -0800

    Add edit and submit functionality on all components

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,55 +1,67 @@
 import "../styles/style.css";
 import App from "../App.jsx";
+import { useState } from "react";
 
 const General = ({ generalInfo, setGeneralInfo }) => {
+  const [isEditing, setIsEditing] = useState(true);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const { name, value } = e.target;
-
-    setGeneralInfo((prevObj) => ({ ...prevObj, [name]: value }));
+    setIsEditing(false);
   };
+
+  const handleEdit = () => setIsEditing(true);
 
   return (
     <div className="sub-container">
       <h2>General Information</h2>
-      <form action="" onSubmit={handleSubmit}>
-        <div className="input-label">
-          <label htmlFor="name">Name: </label>
-          <input
-            type="text"
-            name="name"
-            value={generalInfo.name}
-            onChange={(e) => {
-              setGeneralInfo({ ...generalInfo, name: e.target.value });
-            }}
-          />
-        </div>
+      {isEditing ? (
+        <form action="" onSubmit={handleSubmit}>
+          <div className="input-label">
+            <label htmlFor="name">Name: </label>
+            <input
+              type="text"
+              name="name"
+              value={generalInfo.name}
+              onChange={(e) => {
+                setGeneralInfo({ ...generalInfo, name: e.target.value });
+              }}
+            />
+          </div>
 
-        <div className="input-label">
-          <label htmlFor="email">Email: </label>
-          <input
-            type="email"
-            name="email"
-            value={generalInfo.email}
-            onChange={(e) => {
-              setGeneralInfo({ ...generalInfo, email: e.target.value });
-            }}
-          />
-        </div>
+          <div className="input-label">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="email"
+              name="email"
+              value={generalInfo.email}
+              onChange={(e) => {
+                setGeneralInfo({ ...generalInfo, email: e.target.value });
+              }}
+            />
+          </div>
 
-        <div className="input-label">
-          <label htmlFor="num">Phone Number: </label>
-          <input
-            type="number"
-            name="num"
-            value={generalInfo.phone}
-            onChange={(e) => {
-              setGeneralInfo({ ...generalInfo, phone: e.target.value });
-            }}
-          />
+          <div className="input-label">
+            <label htmlFor="num">Phone Number: </label>
+            <input
+              type="number"
+              name="num"
+              value={generalInfo.phone}
+              onChange={(e) => {
+                setGeneralInfo({ ...generalInfo, phone: e.target.value });
+              }}
+            />
+          </div>
+          <button type="Submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>Name: {generalInfo.name}</p>
+          <p>Email: {generalInfo.email}</p>
+          <p>Phone Number: {generalInfo.phone}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
-        <button type="Submit">Submit</button>
-      </form>
+      )}
     </div>
   );
 };
--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,72 +1,88 @@
 import "../styles/style.css";
+import { useState } from "react";
 
 const Practice = ({ experience, setExperience }) => {
+  const [isEditing, setIsEditing] = useState(true);
+
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const { name, value } = e.target;
-    setExperience((prevObj) => ({ ...prevObj, [name]: value }));
+    setIsEditing(false);
   };
+
+  const handleEdit = () => setIsEditing(true);
 
   return (
     <div className="sub-container">
       <h2>Practical Experience</h2>
-      <form action="" onSubmit={handleSubmit}>
-        <div className="input-label">
-          <label htmlFor="company">Company Name: </label>
-          <input
-            type="text"
-            id="company"
-            value={experience.company}
-            onChange={(e) =>
-              setExperience({ ...experience, company: e.target.value })
-            }
-          />
-        </div>
+      {isEditing ? (
+        <form action="" onSubmit={handleSubmit}>
+          <div className="input-label">
+            <label htmlFor="company">Company Name: </label>
+            <input
+              type="text"
+              id="company"
+              value={experience.company}
+              onChange={(e) =>
+                setExperience({ ...experience, company: e.target.value })
+              }
+            />
+          </div>
 
-        <div className="input-label">
-          <label htmlFor="position">Position Title: </label>
-          <input
-            type="text"
-            id="position"
-            value={experience.position}
-            onChange={(e) =>
-              setExperience({ ...experience, position: e.target.value })
-            }
-          />
-        </div>
+          <div className="input-label">
+            <label htmlFor="position">Position Title: </label>
+            <input
+              type="text"
+              id="position"
+              value={experience.position}
+              onChange={(e) =>
+                setExperience({ ...experience, position: e.target.value })
+              }
+            />
+          </div>
 
-        <div className="input-label">
-          <label htmlFor="responsibilities">
-            Main Responsibilities of the Job:
-          </label>
-          <textarea
-            name=""
-            id="responsibilities"
-            cols="50"
-            rows="10"
-            value={experience.responsibilities}
-            onChange={(e) =>
-              setExperience({ ...experience, responsibilities: e.target.value })
-            }
-          ></textarea>
-        </div>
+          <div className="input-label">
+            <label htmlFor="responsibilities">
+              Main Responsibilities of the Job:
+            </label>
+            <textarea
+              name=""
+              id="responsibilities"
+              cols="50"
+              rows="10"
+              value={experience.responsibilities}
+              onChange={(e) =>
+                setExperience({
+                  ...experience,
+                  responsibilities: e.target.value,
+                })
+              }
+            ></textarea>
+          </div>
 
-        <div className="input-label">
-          <label htmlFor="date">
-            The duration of your employment in the company:
-          </label>
-          <input
-            type="date"
-            id="school"
-            value={experience.duration}
-            onChange={(e) =>
-              setExperience({ ...experience, duration: e.target.value })
-            }
-          />
+          <div className="input-label">
+            <label htmlFor="date">
+              The duration of your employment in the company:
+            </label>
+            <input
+              type="date"
+              id="school"
+              value={experience.duration}
+              onChange={(e) =>
+                setExperience({ ...experience, duration: e.target.value })
+              }
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      ) : (
+        <div>
+          <p>Company Name: {experience.company}</p>
+          <p>Position Title: {experience.position}</p>
+          <p>Job Responsibilities: {experience.responsibilities}</p>
+          <p>Duration of Employment: {experience.duration}</p>
+          <button onClick={handleEdit}>Edit</button>
         </div>
-        <button type="submit">Submit</button>
-      </form>
+      )}
     </div>
   );
 };
